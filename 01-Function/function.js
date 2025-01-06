aa8f044bc1202e7c66c9eaace49e8b93ea92e77a
Ahmad Faizal <ahmadfaizalarrosyid@gmail.com>
2025-01-06 20:43:57 +0700

    Function Regular

---
+++ 01-Function/function.js
@@ -0,0 +1,15 @@
+const temperatureInCelsius = 40;
+
+// Function Regular
+function convertCelsiusToFahrenheit(temperature) {
+    const temperatureInFahrenheit = (temperature * 9 / 5) + 32;
+
+    console.log('Hasil konversi: ' + temperatureInFahrenheit);
+}
+
+// Akan menjalankan isi function
+convertCelsiusToFahrenheit(temperatureInCelsius);
+
+// Hanya menampilkan nilai function
+// console.log(convertCelsiusToFahrenheit);
+
